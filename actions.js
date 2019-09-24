0d65fc7bfd4f21c1b6be706a6771be091c0d1bd4
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-09-24 15:06:02 -0600

    created actions.js for all of the actions we need to use with our character excluding things like flight

---
+++ actions.js
@@ -0,0 +1,189 @@
+require("dotenv").config();
+const axios = require("axios");
+const token = process.env.TOKEN;
+
+const movement = direction => {
+    axios
+        .create({
+            headers: {
+                Authorization: token,
+                "Content-Type": "application/json"
+            }
+        })
+        // You Will need to use the initialize endpoint before you can start moving around
+        .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/move/", {
+            direction: direction
+        })
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+const treasurePickup = () => {
+    axios
+        .create({
+            headers: {
+                Authorization: token,
+                "Content-Type": "application/json"
+            }
+        })
+        .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/take/", {
+            name: "treasure"
+        })
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+const treasureDrop = () => {
+    axios
+        .create({
+            headers: {
+                Authorization: token,
+                "Content-Type": "application/json"
+            }
+        })
+        .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/drop/", {
+            name: "treasure"
+        })
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+const treasureSell = () => {
+    axios
+        .create({
+            headers: {
+                Authorization: token,
+                "Content-Type": "application/json"
+            }
+        })
+        .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/", {
+            name: "treasure"
+        })
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+const treasureSellConfirm = () => {
+    axios
+        .create({
+            headers: {
+                Authorization: token,
+                "Content-Type": "application/json"
+            }
+        })
+        .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/sell/", {
+            name: "treasure",
+            confirm: "yes"
+        })
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+const examine = itemOrPlayerName => {
+    axios
+        .create({
+            headers: {
+                Authorization: token,
+                "Content-Type": "application/json"
+            }
+        })
+        .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/examine/", {
+            name: itemOrPlayerName
+        })
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+const equipClothing = nameOfWearable => {
+    axios
+        .create({
+            headers: {
+                Authorization: token,
+                "Content-Type": "application/json"
+            }
+        })
+        .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/wear/", {
+            name: nameOfWearable
+        })
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+const nameChange = newName => {
+    axios
+        .create({
+            headers: {
+                Authorization: token,
+                "Content-Type": "application/json"
+            }
+        })
+        .post(
+            "https://lambda-treasure-hunt.herokuapp.com/api/adv/change_name/",
+            {
+                name: newName
+            }
+        )
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+const pray = () => {
+    axios
+        .create({
+            headers: {
+                Authorization: token,
+                "Content-Type": "application/json"
+            }
+        })
+        .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/change_name/")
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+module.exports = {
+    movement,
+    treasurePickup,
+    treasureDrop,
+    treasureSell,
+    treasureSellConfirm,
+    examine,
+    equipClothing,
+    nameChange,
+    pray
+};
--- index.js
+++ index.js
@@ -1,21 +1,4 @@
 require("dotenv").config();
-const axios = require("axios");
-const token = process.env.TOKEN;
+const Actions = require("./actions.js");
 
-axios
-    .create({
-        headers: {
-            Authorization: token,
-            "Content-Type": "application/json"
-        }
-    })
-    // You Will need to use the initialize endpoint before you can start moving around
-    .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/move/", {
-        direction: "s"
-    })
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+Actions.movement("n");
