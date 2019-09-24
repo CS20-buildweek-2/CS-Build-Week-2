b4f16b6d2a91334886232a3dbf0a4ffb5a19d61e
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-09-24 14:36:52 -0600

    changes

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const token = process.env.TOKEN;
 axios
     .create({
         headers: {
-            // Authorization: "ADD YOUR TOKEN HERE!",
+            Authorization: token,
             "Content-Type": "application/json"
         }
     })
     // You Will need to use the initialize endpoint before you can start moving around
     .post("https://lambda-treasure-hunt.herokuapp.com/api/adv/move/", {
-        direction: "n"
+        direction: "s"
     })
     .then(res => {
         console.log(res.data);
