da26d5e18c5e738b0bc2b112ae32ed481e840b2d
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-09-25 16:20:31 -0400

    a little improvement

--- index.js
+++ index.js
@@ -11,19 +11,29 @@ async function cooldownreq(endpoint, method, data) {
     try {
         let res = await axios({
             baseURL: 'https://lambda-treasure-hunt.herokuapp.com/api/',
-            headers: { Authorization: `Token ${process.env.TOKEN}` },
+            headers: {
+                Authorization: `Token ${process.env.TOKEN}`,
+                'content-Type': 'application/json'
+            },
             method: `${method}`,
-            url:  `${endpoint}`,
-            data: `${data}`
-        });
+            url: `${endpoint}`,
+            data: JSON.stringify(data)
+        })
+        console.log(res.data)
+                res = await res.data
         await sleep(res.cooldown * 1000)
-        console.log(res)
-      } catch (err) {
-
+        return res
+    } catch (err) {
         console.error(err)
-      }
+    }
 }
 
-function main(){cooldownreq("adv/init/","get")}
+async function main() {
+    const init = await cooldownreq('adv/init', 'get')
+    console.log("Current State: " + init)
+//    const player = await callEndpointAfterCD('adv/status', 'post')    
+//    let current_room = await callEndpointAfterCD('adv/init', 'get')
 
+    //    let current_room = await cooldownreq('adv/init', 'get')
+}
 main()
