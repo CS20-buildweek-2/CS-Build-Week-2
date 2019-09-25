fc59179aa65f6510c47b44971a2632d1d04fe2f5
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-09-25 17:24:28 -0400

    filewrite function

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 require("dotenv").config();
+const fs = require('fs');
 const axios = require("axios");
 const token = process.env.TOKEN;
+
+function writeFile(filename, data) {
+    fs.writeFile(
+        filename,
+        JSON.stringify(data),
+        { flag: "a" },
+        function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        }
+    )
+}
 
 
 function sleep(ms) {
@@ -19,8 +33,8 @@ async function cooldownreq(endpoint, method, data) {
             url: `${endpoint}`,
             data: JSON.stringify(data)
         })
-        console.log(res.data)
-                res = await res.data
+        //        console.log(res.data)
+        res = await res.data
         await sleep(res.cooldown * 1000)
         return res
     } catch (err) {
@@ -29,11 +43,15 @@ async function cooldownreq(endpoint, method, data) {
 }
 
 async function main() {
-    const init = await cooldownreq('adv/init', 'get')
-    console.log("Current State: " + init)
-//    const player = await callEndpointAfterCD('adv/status', 'post')    
-//    let current_room = await callEndpointAfterCD('adv/init', 'get')
+    //    const init = await cooldownreq('adv/init', 'get')
+    //    console.log(init)
+    //    const player = await callEndpointAfterCD('adv/status', 'post')
+    //    let current_room = await callEndpointAfterCD('adv/init', 'get')
 
     //    let current_room = await cooldownreq('adv/init', 'get')
+    test = {"test":"test"}
+    writeFile("test.json", test)
+
+
 }
 main()
